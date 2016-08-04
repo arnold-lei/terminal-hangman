363d34d970496e4d69946c3f9137626543e1ccc3
Arnold Lei <arnold.lei88@gmail.com>
2016-08-04 17:32:17 -0400

    got the program to keep asking the user for new guesses.

--- game.js
+++ game.js
@@ -3,13 +3,15 @@ var term = require('terminal-kit').terminal;
 var wurd = require('./word');
 var letter = require('./letter');
 var inquirer = require('inquirer');	
-var userGuess;
+var userGuess = [];
 
 function generateWord(){
 	urban.random().first(function(werd) {
 		term.clear();
 		var entry = new wurd(werd.word, werd.definition);
 	    letter(entry.word);
+	    console.log('Word: ' + werd.word);
+	    console.log('Definition: ' + werd.definition)
 	    guess();
 	});	
 };
@@ -19,24 +21,21 @@ var question = [{
     name: 'guess',
     y: 1,
     message: 'Welcome to Hangman! Please guess a letter: ',
-    validate: function(value){
-    	if(isNan(vaule) == true){
-    		return true;
-    	} else {
-    		return false;
-    		guess.push(value);
-    		console.log(guess);
-    	}
-    }
+    // validate: function(value){
+    // 	if(isNan(vaule) == true){
+    // 		return true;
+    // 	} else {
+    // 		return false;
+    // 		guess.push(value);
+    // 		console.log(guess);
+    // 	}
+    // }
 }];
 
 var guess = function(){
 	inquirer.prompt(question).then(function (answers) {
-	  userGuess.push(answers.guess)
-	  term(answer.guess)
-	}).then(function(ans){
-		guess.push(ans);
-		term(guess)
+		console.log(userGuess)
+		userGuess.push(answers.guess);
 		guess()
 	});
 }
